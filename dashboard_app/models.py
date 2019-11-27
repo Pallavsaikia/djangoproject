16901a2692536383c20ceedb38b55f1336e0c723
paul_siqes <pallavsaikia57@gmail.com>
2019-11-27 14:11:15 +0530

    displaying pending queries

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -5,24 +5,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Appointment(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    day_of_appointment = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    day_of_appointment = models.DateTimeField()
 
     def __str__(self):
         return str(self.day_of_appointment)
 
 
 class Query(models.Model):
-    asked_by = models.OneToOneField(User, on_delete=models.CASCADE)
-    asked_On = models.DateField()
+    asked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    asked_On = models.DateTimeField(auto_now_add=True, blank=True)
     question = models.TextField()
+    replied = models.BooleanField(default=False)
 
     def __str__(self):
         return self.question
 
 
 class Answer(models.Model):
-    replied_by = models.OneToOneField(User, on_delete=models.CASCADE)
-    replied_to = models.OneToOneField(Query, on_delete=models.CASCADE)
-    replied_on = models.DateField()
+    replied_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    replied_to = models.ForeignKey(Query, on_delete=models.CASCADE)
+    replied_on = models.DateTimeField(auto_now_add=True, blank=True)
     reply = models.TextField()
--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
+from dashboard_app.models import Query
 
 
 class Dashboard(View):
 
     @method_decorator(login_required(login_url='/'))
     def get(self, request):
-        return render(request, 'dashboard.html')
+        query = Query.objects.filter(replied=False)
+        context_send = {'queries': query}
+        return render(request, 'dashboard.html', context=context_send)
 
     def post(self, request):
         pass
