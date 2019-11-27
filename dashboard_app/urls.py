033ef7428ee7c3dc02b0585d845e79d4f751e97c
paul_siqes <pallavsaikia57@gmail.com>
2019-11-27 22:05:53 +0530

    reply to pending queries

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from dashboard_app import views
 
 urlpatterns = [
-    url(r'^$', views.Dashboard.as_view(),name='dashboard'),
+    url(r'^$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^query/(?P<value>\d+)/$', views.QueryView.as_view(), name='query')
+
 ]
--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
-from dashboard_app.models import Query
+from dashboard_app.models import Query, Answer
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 class Dashboard(View):
@@ -17,3 +19,24 @@ class Dashboard(View):
 
     def post(self, request):
         pass
+
+
+class QueryView(View):
+
+    @method_decorator(login_required(login_url='/'))
+    def get(self, request, value):
+        query = Query.objects.get(pk=value)
+        query_reply = Answer.objects.filter(replied_to=value).order_by('-replied_on')
+        context_send = {'queries': query, 'replies': query_reply}
+        return render(request, 'answer_query.html', context=context_send)
+
+    @method_decorator(login_required(login_url='/'))
+    def post(self, request, value):
+        user = User.objects.get(pk=request.user.id)
+        question = Query.objects.get(pk=value)
+        answer = request.POST['answer']
+
+        Answer(replied_by=user, replied_to=question, reply=answer).save()
+        question.replied = True
+        question.save()
+        return HttpResponseRedirect(reverse('query', args=(value,)))
