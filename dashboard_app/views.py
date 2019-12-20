91e0cf246ba861e8d0dcb8b7de9a5d9bbccf5626
paul_siqes <pallavsaikia57@gmail.com>
2019-12-20 15:58:40 +0530

    pending appointment notification

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
-from dashboard_app.models import Query, Answer
+from dashboard_app.models import Query, Answer, Appointment
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
@@ -14,7 +14,7 @@ class Dashboard(View):
     @method_decorator(login_required(login_url='/'))
     def get(self, request):
         query = Query.objects.filter(replied=False).filter(closed=False).order_by('-asked_On')
-        context_send = {'queries': query}
+        context_send = {'queries': query,'pending': getAppointment()}
         return render(request, 'dashboard.html', context=context_send)
 
     def post(self, request):
@@ -27,7 +27,7 @@ class QueryViewThread(View):
     def get(self, request, value):
         query = Query.objects.get(pk=value)
         query_reply = Answer.objects.filter(replied_to=value).order_by('replied_on')
-        context_send = {'queries': query, 'replies': query_reply}
+        context_send = {'queries': query, 'replies': query_reply,'pending': getAppointment()}
         return render(request, 'answer_query.html', context=context_send)
 
     @method_decorator(login_required(login_url='/'))
@@ -45,9 +45,17 @@ class QueryViewThread(View):
 class QueryView(View):
     @method_decorator(login_required(login_url='/'))
     def get(self, request):
-        query = Query.objects.order_by('-asked_On','replied')
-        context_send = {'queries': query}
+        query = Query.objects.order_by('-asked_On', 'replied')
+        context_send = {'queries': query, 'pending': getAppointment()}
         return render(request, 'query.html', context=context_send)
 
     def post(self, request):
         pass
+
+
+def getAppointment():
+    count = Appointment.objects.filter(appointed=False).count()
+    if count >= 1:
+        return True
+    else:
+        return False
